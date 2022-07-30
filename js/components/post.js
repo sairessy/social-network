de4423c856d959a485dc7b0cd6ffc246ad34ecf2
sairessy <sairessy@gmail.com>
2022-07-30 06:20:59 -1100

    Make it beauty

--- js/components/post.js
+++ js/components/post.js
@@ -1,8 +1,19 @@
-const Post = ({ _id, text }) => {
+const Post = ({ _id, text, cratedAt, user }) => {
   return `
     <div class="post">
       <div class="post-header">
-        <p class="publisher"></p>
+      <div id="form-post-header">
+      <div>
+        <div class="avatar">
+          <img src="assets/img/user-alt-solid.svg" alt="user-image" />
+        </div>
+      </div>
+      <div>
+        <h4>${user}</h4>
+        <p>${new Date(cratedAt)}</p>
+      </div>
+    </div>
+
       </div>
       <div class="post-body">
         <p>
@@ -10,8 +21,9 @@ const Post = ({ _id, text }) => {
         </p>
       </div>
       <div class="post-footer">
-        <span><i class="la la-hand-peace"></i><b>33</b></span>
-        <span><i class="la la-comment"></i><b>11</b></span>
+        <span class="btn-like"><i class="la la-heart-o"></i><b>33</b></span>
+        <span class="btn-comment"><i class="la la-comment"></i><b>11</b></span>
+        <span class="btn-share"><i class="la la-share"></i><b>11</b></span>
       </div>
     </div>
   `;
